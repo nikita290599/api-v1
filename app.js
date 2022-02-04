68e3cf6a439611eba551b4a9b85a83de4a96ba09
nikita290599 <2018168@iiitdmj.ac.in>
2022-02-04 11:59:57 +0530

    whatsapp-text-to json

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const AdmZip = require("adm-zip");
+const whatsapp = require('whatsapp-chat-parser');
 
 const app = express();
 
@@ -13,12 +14,12 @@ var fs = require('fs');
 
 // For Multer Storage
 var multerStorage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, path.join(__dirname, 'my_uploads'));
-    },
-    filename: function (req, file, callback) {
-        callback(null, Date.now() + '_' + file.originalname);
-    }
+  destination: function (req, file, callback) {
+    callback(null, path.join(__dirname, 'my_uploads'));
+  },
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + '_' + file.originalname);
+  }
 });
 
 // For Single File upload
@@ -31,72 +32,76 @@ var multerMultipleUpload = multer({ storage: multerStorage }).array("multipleIma
 
 //route for single text file upload
 app.post("/api/v1/txt_file", multerSigleUpload.single('text'), function (req, res) {
-    const file = req.file;
-    const fileName =req.file.filename; 
-    console.log("FILENAME: ", req.file.filename);
-    if (!file) {
-        return res.end("Please choose file to upload!");
-    }
-    fs.readFile(__dirname + '/my_uploads/' + fileName, "utf8", (error, data) => {
-        //Add your error handling (if(error))
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({data}));
-        
+  const file = req.file;
+  const fileName = req.file.filename;
+  console.log("FILENAME: ", req.file.filename);
+  if (!file) {
+    return res.end("Please choose file to upload!");
+  }
+
+  const fileContents = fs.readFileSync(__dirname + '/my_uploads/' + fileName, 'utf8');
+  whatsapp
+    .parseString(fileContents)
+    .then(messages => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({messages}));
+      // Do whatever you want with messages
+    })
+    .catch(err => {
+      // Something went wrong
     });
-    // req.app.locals.uploadStatus = true;
-    // res.redirect('/');
 });
 
 
 
 
-  // Route for zip file upload
+// Route for zip file upload
 app.post("/api/v1/zip_file", multerSigleUpload.single('zip'), function (req, res) {
-    const files = req.file;
-    const fileName =req.file.filename; 
-    console.log("FILENAME: ", fileName);
+  const files = req.file;
+  const fileName = req.file.filename;
+  console.log("FILENAME: ", fileName);
 
-    const outputDir =path.join(__dirname,'my_uploads',fileName.slice(0,-4));
-    // UNZIPPING FILE
-    async function extractArchive(filepath) {
-        try {
-          const zip = new AdmZip(filepath);
-          
-          zip.extractAllTo(outputDir,true);
-         
-          console.log(`Extracted to "${outputDir}" successfully`);
-        } catch (e) {
-          console.log(`Something went wrong. ${e}`);
-        }
-      }
-      
-    extractArchive(__dirname+"/my_uploads/"+fileName).then(()=>{
-        let arrayList=[]; 
-        let filenames = fs.readdirSync(outputDir);
-        var temp={};
-        console.log("\nFilenames in directory:");
-       filenames.forEach((file) => {
-            fs.readFile(outputDir +"/"+ file, "utf8", (error, data) => {
-                
-                //Add your error handling (if(error))
-                temp={
-                    'name':file,
-                    'content':data
-                }
-                arrayList.push(temp);
-                console.log(temp);
-            });
-        });
-        if(arrayList[0]){
-            
-            res.end(JSON.stringify({arrayList}));
-        }
-       
-    })
+  const outputDir = path.join(__dirname, 'my_uploads', fileName.slice(0, -4));
+  // UNZIPPING FILE
+  async function extractArchive(filepath) {
+    try {
+      const zip = new AdmZip(filepath);
 
-    
-   
-    // res.redirect('/');
+      zip.extractAllTo(outputDir, true);
+
+      console.log(`Extracted to "${outputDir}" successfully`);
+    } catch (e) {
+      console.log(`Something went wrong. ${e}`);
+    }
+  }
+
+  extractArchive(__dirname + "/my_uploads/" + fileName).then(() => {
+    let arrayList = [];
+    let filenames = fs.readdirSync(outputDir);
+    var temp = {};
+    console.log("\nFilenames in directory:");
+    filenames.forEach((file) => {
+      fs.readFile(outputDir + "/" + file, "utf8", (error, data) => {
+
+        //Add your error handling (if(error))
+        temp = {
+          'name': file,
+          'content': data
+        }
+        arrayList.push(temp);
+        console.log(temp);
+      });
+    });
+    if (arrayList[0]) {
+
+      res.end(JSON.stringify({ arrayList }));
+    }
+
+  })
+
+
+
+  // res.redirect('/');
 });
 
 // //route for multiple file upload
@@ -112,5 +117,5 @@ app.post("/api/v1/zip_file", multerSigleUpload.single('zip'), function (req, res
 
 // Server Listening
 app.listen(3000, () => {
-    console.log('Server is running at port 3000');
+  console.log('Server is running at port 3000');
 });
